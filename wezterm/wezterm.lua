5e2e34d6cff5c3ecf4a5e7621f5a373216deef95
Yui Tanabe <2323102@asia-u.ac.jp>
2023-11-08 23:30:46 +0900

    feat(wez/windows): force default to user cmd

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,3 +1,11 @@
 local wezterm = require("wezterm")
 
-return {}
+local Config = {}
+
+Config.font = wezterm.font "Hack Nerd Font Mono"
+if string.match(wezterm.target_triple, "msvc") then
+Config.default_prog = { "cmd.exe" }
+end
+
+return Config
+
