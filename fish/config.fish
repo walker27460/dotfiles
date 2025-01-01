ad40aea21012134b97651b763fa59aa40dc22557
Yui Tanabe <walker27460@users.noreply.github.com>
2025-01-02 01:26:09 +0900

    feat: add brewfile

--- fish/config.fish
+++ fish/config.fish
@@ -3,8 +3,21 @@ if status is-interactive
 end
 
 if ! functions -q fisher
-  curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
+end
+
+function setup
+    xcode-select --install
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 end
 
 fish_add_path /opt/homebrew/bin
 
+function ide
+    tmux split-window -v -l 30%
+    tmux split-window -h -l 66%
+    tmux split-window -h -l 50%
+end
+
+set -x SSH_AUTH_SOCK /Users/walker27460/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
+alias md="mkdir -p"
