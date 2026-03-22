50aa6eaa0d28a5f4dc82b437b01fb6350bc3abb0
Yui Tanabe <walker27460@users.noreply.github.coml>
2026-03-22 15:09:56 +0900

    feat: migrate to custom config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,53 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+vim.cmd [[set et ts=2 sts=-1 sw=2]]
+vim.cmd [[colorscheme catppuccin]]
+
+vim.pack.add {
+  -- Language engine & snippets
+  { src = "https://github.com/ms-jpq/coq_nvim", name = "coq.nvim" },
+  { src = "https://github.com/ms-jpq/coq.artifacts" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+
+  -- Tabline
+  { src = "https://github.com/nvim-lualine/lualine.nvim" },
+
+  -- LLM support
+  { src = "https://github.com/nvim-lua/plenary.nvim" },
+  { src = "https://github.com/olimorris/codecompanion.nvim" },
+
+  -- File tree explorer
+  { src = "https://github.com/ms-jpq/chadtree", name = "chadtree.nvim" }
+}
+
+local coq = require("coq")
+coq.Now()
+
+local lualine = require("lualine")
+lualine.setup {
+}
+
+local cc = require("codecompanion")
+cc.setup {
+  adapters = {
+    http = {
+      huggingface = function()
+        return require("codecompanion.adapters").extend("huggingface", {
+          env = {
+            api_key = vim.env.HF_API_KEY
+          }
+        })
+      end
+    }
+  },
+  interactions = {
+    chat = {
+      adapter = {
+        name = "huggingface",
+        model = "Qwen/Qwen3.5-397B-A17B"
+      }
+    },
+    cmd = {
+      adapter = "huggingface"
+    }
+  }
+}
+
